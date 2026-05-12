d48e13cf4e13d6bce073a929d5317c7022819902
Tushar Kalra <TUSHARKALRA307@GMAIL.COM>
2026-05-12 09:51:38 +0530

    Remove Tauri integration and update project description for clarity

--- src-tauri/src/main.rs
+++
@@ -1,8 +0,0 @@
-// Prevents additional console window on Windows in release
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-mod commands;
-
-fn main() {
-    zenith_canvas_lib::run();
-}
